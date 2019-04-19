ad053bfd065a84643e3c8dc274829219927b2efa
Shubham Agarwal <shubham.agarwal@railyatri.in>
2019-04-20 03:05:26 +0530

    1. Added notification support for better visibility 2. Added GEOFENCE_TRANSITION_DWELL support 3. supported AS3.4 and latest dependencies, compileSdkVersion and targetSdkVersion

--- app/src/main/java/com/app/androidkt/geofencing/Constants.java
+++ app/src/main/java/com/app/androidkt/geofencing/Constants.java
@@ -12,7 +12,7 @@ public class Constants {
 
 
     public static final String GEOFENCE_ID_STAN_UNI = "STAN_UNI";
-    public static final float GEOFENCE_RADIUS_IN_METERS = 100;
+    public static final float GEOFENCE_RADIUS_IN_METERS = 1000;
 
     /**
      * Map for storing information about stanford university in the Stanford.
@@ -21,6 +21,9 @@ public class Constants {
 
     static {
         // stanford university.
-        AREA_LANDMARKS.put(GEOFENCE_ID_STAN_UNI, new LatLng(37.427476, -122.170262));
+//        AREA_LANDMARKS.put(GEOFENCE_ID_STAN_UNI, new LatLng(37.427476, -122.170262));
+
+        // Add your lat long here...
+        AREA_LANDMARKS.put(GEOFENCE_ID_STAN_UNI, new LatLng(28.6337874, 77.3554873));
     }
 }
